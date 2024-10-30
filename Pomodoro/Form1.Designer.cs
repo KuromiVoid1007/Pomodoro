5a88aa9ff9cf3f1e220e91c2f48211fb4136676e
Роман Порсев <coolvip1007@mail.ru>
2024-10-31 00:02:57 +0500

    Добавьте файлы проекта.

---
+++ Pomodoro/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Pomodoro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            buttonStart = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(102, 110);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(87, 263);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(75, 23);
+            buttonStart.TabIndex = 1;
+            buttonStart.Text = "button1";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.Text = "notifyIcon1";
+            notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(244, 341);
+            Controls.Add(buttonStart);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Button buttonStart;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ Pomodoro/Form1.cs
@@ -0,0 +1,110 @@
+using System.Media;
+using System.Windows.Forms;
+
+
+namespace Pomodoro
+{
+    public partial class Form1 : Form
+    {
+        private int timeLeft; // Время в секундах
+
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateTimeDisplay();
+
+
+            // Настройка NotifyIcon
+            notifyIcon1.Icon = Properties.Resources.favicon; // Укажите свой файл иконки
+            notifyIcon1.Text = "Мое приложение"; // Подсказка при наведении на иконку
+            notifyIcon1.Visible = false; // Иконка не видна по умолчанию
+
+            // Настройка контекстного меню
+            var contextMenu = new ContextMenuStrip();
+            contextMenu.Items.Add("Открыть", null, ShowForm);
+            contextMenu.Items.Add("Выход", null, ExitApplication);
+            notifyIcon1.ContextMenuStrip = contextMenu;
+
+            // Обработка события двойного щелчка по иконке
+            notifyIcon1.DoubleClick += ShowForm;
+        }
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            InitializeCountdown(2); // Запуск таймера на 60 секунд по нажатию кнопки
+        }
+
+        private void InitializeCountdown(int seconds)
+        {
+            timeLeft = seconds;
+            timer1.Interval = 1000; // 1000 миллисекунд = 1 секунда
+            timer1.Tick += Timer1_Tick;
+            timer1.Start();
+            UpdateTimeDisplay();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--; // Уменьшаем время
+                UpdateTimeDisplay();
+            }
+            else
+            {
+                PlaySound();
+                timer1.Stop();
+                MessageBox.Show("Время вышло!");
+            }
+        }
+
+        private void UpdateTimeDisplay()
+        {
+            label1.Text = TimeSpan.FromSeconds(timeLeft).ToString(@"mm\:ss");
+        }
+
+        private void PlaySound()
+        {
+            try
+            {
+                using (SoundPlayer player = new SoundPlayer(Pomodoro.Properties.Resources.zvon)) // Замените "alert.wav" на путь к вашему файлу
+                {
+                    player.Play(); // Воспроизведение звука
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка воспроизведения звука: " + ex.Message);
+            }
+        }
+
+        // Метод для показа формы
+        private void ShowForm(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+        }
+
+        // Метод для выхода из приложения
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+
+        // Событие Resize для сворачивания в трей
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide(); // Скрыть форму
+                notifyIcon1.Visible = true; // Показать иконку в трее
+                notifyIcon1.ShowBalloonTip(1000, "Приложение свернуто", "Нажмите, чтобы развернуть", ToolTipIcon.Info);
+            }
+        }
+
+    }
+}
